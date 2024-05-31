d0329a76beba6d1747e23972e579ac6dbad90527
LifeIsOne <didtmdgh258@gmail.com>
2024-05-31 14:24:29 +0900

    Kakao OAuth를 통한 회원가입 및 로그인, 사용자 정보표시

--- src/main/java/shop/mtcoding/loginapp/user/UserController.java
+++ src/main/java/shop/mtcoding/loginapp/user/UserController.java
@@ -13,19 +13,31 @@ public class UserController {
     private final UserService us;
     private final HttpSession session;
 
+    // http://localhost:8080/oauth/callback?code=3u9fk
+    @GetMapping("/oauth/callback")
+    public String oauthCallback(String code) {
+        System.out.println("우와 콜백 됐다! : " + code);
+        User sessionUser = us.카카오로그인(code);
+        session.setAttribute("sessionUser", sessionUser);
+        return "redirect:/shop";
+    }
+
     @GetMapping("/join-form")
     public String joinForm() {
         return "join-form";
     }
+
     @GetMapping("/login-form")
     public String loginForm() {
         return "login-form";
     }
+
     @PostMapping("/join")
     public String join(String username, String password, String email) {
         us.회원가입(username, password, email);
         return "redirect:/login-form";
     }
+
     @PostMapping("/login")
     public String login(String username, String password, String email) {
         User sessionUser = us.로그인(username, password);
